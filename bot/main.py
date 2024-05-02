b15bf15fe48ce5d6682ee25866ccaa7229cca1ce
Igor Gorbenko <igor_gorbenko@list.ru>
2024-05-02 17:08:54 +0400

    Add contact request

--- bot/main.py
+++ bot/main.py
@@ -1,148 +1,14 @@
-# import logging
-#
-# from telegram import Update, ReplyKeyboardMarkup, MenuButtonCommands, InlineKeyboardButton, InlineKeyboardMarkup
-# from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, CallbackContext
-#
-# from credentials import TOKEN
-#
-# # Определение стадий диалога
-# BRANCH, PERSONAL_INFO, DOCTOR, SCHEDULE, DATE = range(5)
-#
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#     level=logging.INFO
-# )
-#
-#
-# # async def start(update: Update, context):
-# #     keyboard = [
-# #         [InlineKeyboardButton("Новая запись", callback_data='new_appointment')],
-# #         [InlineKeyboardButton("Посмотреть мои записи", callback_data='view')]
-# #     ]
-# #
-# #     telegram_user_first_name = update.effective_user.first_name
-# #     reply_markup = InlineKeyboardMarkup(keyboard)
-# #     await update.message.reply_text(
-# #         f'Здравствуйте, {telegram_user_first_name}! Я бот для записи на прием к врачу. '
-# #         f'Вы можете выбрать одну из следующих команд:',
-# #         # reply_markup=ReplyKeyboardMarkup(main_menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
-# #         reply_markup=reply_markup
-# #     )
-# async def start(update: Update, context: CallbackContext):
-#     keyboard = [
-#         [InlineKeyboardButton("Новая запись", callback_data='new')],
-#         [InlineKeyboardButton("Посмотреть мои записи", callback_data='view')]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     await update.message.reply_text(
-#         f'Здравствуйте, {update.effective_user.first_name}! Я бот для записи на прием к врачу. '
-#         f'Вы можете выбрать одну из следующих команд:',
-#         reply_markup=reply_markup
-#     )
-#
-# async def set_menu_button(bot):
-#     menu_button = MenuButtonCommands('Меню')
-#     await bot.set_chat_menu_button(menu_button=menu_button)
-#
-#
-# async def new(update: Update, context):
-#     reply_keyboard = [['Branch 1', 'Branch 2', 'Branch 3']]
-#     await update.message.reply_text(
-#         'В каком филиале вы хотите записаться?',
-#         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
-#     )
-#     return BRANCH
-#
-# async def view(update: Update, context):
-#     await update.message.reply_text('Вот список ваших предстоящих записей:')
-#     # Здесь код для извлечения и отображения записей пользователя
-#
-# async def branch(update: Update, context):
-#     branch = update.message.text
-#     context.user_data['branch'] = branch
-#     await update.message.reply_text('Пожалуйста, укажите ваше ФИО и контактные данные.')
-#     return PERSONAL_INFO
-#
-#
-# async def personal_info(update: Update, context):
-#     user_info = update.message.text
-#     context.user_data['personal_info'] = user_info
-#     reply_keyboard = [['Doctor A', 'Doctor B', 'Doctor C']]
-#     await update.message.reply_text(
-#         'Выберите врача:',
-#         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-#     )
-#     return DOCTOR
-#
-#
-# async def doctor(update: Update, context):
-#     doctor = update.message.text
-#     context.user_data['doctor'] = doctor
-#     await update.message.reply_text('Выберите дату и время:')
-#     return SCHEDULE
-#
-#
-# async def schedule(update: Update, context):
-#     date = update.message.text
-#     context.user_data['date'] = date
-#
-#     doctor = context.user_data['doctor']
-#     await update.message.reply_text(f'Вы записаны на {date} к доктору {doctor}. Спасибо!')
-#     return ConversationHandler.END
-#
-#
-# def handle_response(text: str) -> str:
-#     proccessed_text: str = text.lower()
-#
-#     if 'оператор' in text:
-#         return 'Наш диалог будет отправлен оператору. Наши клиника свяжется с Вами. Спасибо за обращение'
-#
-#     return 'Не понял ваш запрос, пожалуйста, повторите'
-#
-#
-# async def handle_unknown(update: Update, context: CallbackContext):
-#     await update.message.reply_text('Текст не распознан. Пожалуйста, выберите действие из меню ниже.')
-#     # await start(update, context)  # Повторно вызываем функцию start для показа кнопок.
-#
-#
-# def main():
-#     application = Application.builder().token(TOKEN).build()
-#
-#     conv_handler = ConversationHandler(
-#         entry_points=[CommandHandler('start', start)],
-#         states={
-#             BRANCH: [MessageHandler(filters.TEXT & ~filters.COMMAND, branch)],
-#             PERSONAL_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, personal_info)],
-#             DOCTOR: [MessageHandler(filters.TEXT & ~filters.COMMAND, doctor)],
-#             SCHEDULE: [MessageHandler(filters.TEXT & ~filters.COMMAND, schedule)],
-#         },
-#         fallbacks=[CommandHandler('start', start)]
-#     )
-#
-#     # Обработка команд "Новая запись" и "Посмотреть мои записи"
-#     application.add_handler(CommandHandler('menu', start))
-#     application.add_handler(CommandHandler('help', start))
-#     # application.add_handler(MessageHandler(filters.Regex('^Новая запись$'), new_appointment))
-#     # application.add_handler(MessageHandler(filters.Regex('^Посмотреть мои записи$'), view_appointments))
-#     # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_unknown))
-#
-#     application.add_handler(conv_handler)
-#     application.run_polling()
-#     # application.bot_data.dispatcher.run_async(set_menu_button, application.bot)
-#
-#
-# if __name__ == '__main__':
-#     main()
-
 import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import (
     Application, CommandHandler, ConversationHandler, MessageHandler, filters, CallbackContext, CallbackQueryHandler)
 
 from credentials import TOKEN
 
 # Определение стадий диалога
-BRANCH, PERSONAL_INFO, DOCTOR, SCHEDULE, DATE = range(5)
+# BRANCH, DOCTOR, PERSONAL_INFO, SCHEDULE, DATE = range(5)
+BRANCH, PERSONAL_INFO, CONTACT_INFO, DOCTOR, SCHEDULE = range(5)
+# PERSONAL_INFO, CONTACT = range(2)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -165,6 +31,18 @@ async def start(update: Update, context: CallbackContext):
     )
 
 
+async def contact_handler(update: Update, context: CallbackContext):
+    contact_keyboard = KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    custom_keyboard = [[contact_keyboard]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+    if update.callback_query:
+        await update.callback_query.answer()  # Асинхронно ответить на callback_query
+        await update.callback_query.message.reply_text('Пожалуйста, поделись своим номером телефона.', reply_markup=reply_markup)
+    else:
+        await update.message.reply_text('Пожалуйста, поделись своим номером телефона.', reply_markup=reply_markup)
+
+
 async def help(update: Update, context: CallbackContext):
     keyboard = [
         [InlineKeyboardButton("<-- Назад", callback_data='view')],
@@ -172,19 +50,19 @@ async def help(update: Update, context: CallbackContext):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        f'Мы постараемся почомь Вам как можно скорее, пожалуйста, напишите ниже свой вопрос',
+        f'Мы постараемся помочь Вам как можно скорее, пожалуйста, напишите ниже свой вопрос',
         reply_markup=reply_markup
     )
 
 
 async def new(update: Update, context: CallbackContext):
     keyboard = [
-        [InlineKeyboardButton("Branch 1", callback_data='branch_select,branch_1'),
-         InlineKeyboardButton("Branch 2", callback_data='branch_select,branch_2')],
-        [InlineKeyboardButton("Branch 3", callback_data='branch_select,branch_3'),
-         InlineKeyboardButton("Branch 4", callback_data='branch_select,branch_4')],
-        [InlineKeyboardButton("Branch 5", callback_data='branch_select,branch_5'),
-         InlineKeyboardButton("Branch 6", callback_data='branch_select,branch_6')]
+        [InlineKeyboardButton("Branch 1", callback_data='select_branch,branch_1'),
+         InlineKeyboardButton("Branch 2", callback_data='select_branch,branch_2')],
+        [InlineKeyboardButton("Branch 3", callback_data='select_branch,branch_3'),
+         InlineKeyboardButton("Branch 4", callback_data='select_branch,branch_4')],
+        [InlineKeyboardButton("Branch 5", callback_data='select_branch,branch_5'),
+         InlineKeyboardButton("Branch 6", callback_data='select_branch,branch_6')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query = update.callback_query
@@ -205,27 +83,31 @@ async def branch(update: Update, context: CallbackContext):
     return PERSONAL_INFO
 
 
-async def personal_info(update: Update, context: CallbackContext):
+async def doctor(update: Update, context: CallbackContext):
     query = update.callback_query
     await query.answer()
-    # Использование query.message для получения доступа к тексту сообщения
-    user_info = query.message.text
-    context.user_data['personal_info'] = user_info
     keyboard = [
-        [InlineKeyboardButton("Doctor A", callback_data='doctor_a')],
-        [InlineKeyboardButton("Doctor B", callback_data='doctor_b')],
-        [InlineKeyboardButton("Doctor C", callback_data='doctor_c')]
+        [InlineKeyboardButton("Доктор Игорь", callback_data='doc_select,doctor_a'),
+         InlineKeyboardButton("Доктор Яков", callback_data='doc_select,doctor_b')],
+        [InlineKeyboardButton("Доктор Ринат", callback_data='doc_select,doctor_c')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(text='Выберите врача:', reply_markup=reply_markup)
     return DOCTOR
 
 
-async def doctor(update: Update, context: CallbackContext):
-    doctor = update.message.text
-    context.user_data['doctor'] = doctor
-    await update.message.reply_text('Выберите дату и время:')
-    return SCHEDULE
+async def personal_info(update: Update, context: CallbackContext):
+    query = update.callback_query
+    await query.answer()
+    # Вы можете здесь запросить ФИО пациента, если это необходимо.
+    await query.message.reply_text('Введите ФИО пациента')
+    return PERSONAL_INFO
+
+
+async def request_contact(update: Update, context: CallbackContext):
+    context.user_data['full_name'] = update.message.text  # Сохраняем введенное ФИО
+    await update.message.reply_text('Введите ваш контактный номер')
+    return CONTACT_INFO
 
 
 async def schedule(update: Update, context: CallbackContext):
@@ -249,9 +131,13 @@ async def button_handler(update: Update, context: CallbackContext):
 
     if action == 'new':
         logging.info('New')
-        await new(update, context)
-    elif action == 'branch_select':
+        # await new(update, context)
+        await contact_handler(update, context)
+    elif action == 'select_branch':
         logging.info('Branch selected')
+        await doctor(update, context)
+    elif action == 'doc_select':
+        logging.info('Doc selected')
         await personal_info(update, context)
     elif action == 'view':
         await view_appointments(update, context)
@@ -265,19 +151,24 @@ def main():
     application.add_handler(CommandHandler('menu', start))
     application.add_handler(CommandHandler('help', help))
     application.add_handler(CallbackQueryHandler(button_handler))  # Обработчик нажатий на кнопки
-    # application.add_handler(ConversationHandler(
-    #     entry_points=[CommandHandler('start', start)],
-    #     states={
-    #         BRANCH: [MessageHandler(filters.TEXT & ~filters.COMMAND, branch)],
-    #         PERSONAL_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, personal_info)],
-    #         DOCTOR: [MessageHandler(filters.TEXT & ~filters.COMMAND, doctor)],
-    #         SCHEDULE: [MessageHandler(filters.TEXT & ~filters.COMMAND, schedule)],
-    #     },
-    #     fallbacks=[CommandHandler('start', start)]
-    # ))
+
+    application.add_handler(MessageHandler(filters.CONTACT, contact_handler))
+
+    application.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('personal_info', personal_info)],
+        states={
+            # BRANCH: [MessageHandler(filters.TEXT & ~filters.COMMAND, branch)],
+            PERSONAL_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, personal_info)],
+            CONTACT_INFO: [MessageHandler(filters.TEXT & ~filters.COMMAND, request_contact)],
+            # DOCTOR: [MessageHandler( filters.TEXT & ~filters.COMMAND, doctor)],
+            # SCHEDULE: [MessageHandler(filters.TEXT & ~filters.COMMAND, schedule)],
+        },
+        fallbacks=[CommandHandler('start', start)]
+    ))
     application.run_polling()
 
 
 if __name__ == '__main__':
     main()
+
 
