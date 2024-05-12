600530ab62fc8bd57c8bb380ffa97f82f505c98a
Igor Gorbenko <igor_gorbenko@list.ru>
2024-05-12 20:52:25 +0400

    Somem minor changes

--- bot/doctor.py
+++ bot/doctor.py
@@ -403,53 +403,56 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 async def start(update: Update, context: CallbackContext, from_back=False):
-    # keyboard = [[InlineKeyboardButton("Начать", callback_data='begin')]]
-    message = "Пожалуйста, введите ваше имя:"
-    if from_back:
-        await update.callback_query.edit_message_text(message)
-    else:
+    # Если функция вызвана через /cancel, update.message может быть None
+    if update.message:
         chat_id = update.message.chat_id
-        await update.message.reply_text('Привет! Я бот для записи в стоматологическую клинику')
+        command = update.message.text
+        if command == '/cancel':
+            # Если вызов произошел через команду /cancel, отправляем сообщение о сбросе
+            await update.message.reply_text('Диалог был сброшен. Давайте начнем заново!')
+            await asyncio.sleep(0.5)  # Задержка перед следующим сообщением
+        else:
+            # Приветствие при стандартном вызове команды /start
+            await update.message.reply_text('Привет! Я бот для записи в стоматологическую клинику')
+            await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+            await asyncio.sleep(0.5)  # Задержка для имитации печати
+    else:
+        # Если вызов произошел из callback_query (например, при возврате назад)
+        chat_id = update.callback_query.message.chat_id
+        await update.callback_query.answer()  # Ответ на callback_query
+        await update.callback_query.edit_message_text('Давайте начнем заново! Пожалуйста, введите ваше имя:')
 
-        await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
-        await asyncio.sleep(0.5)
-        await update.message.reply_text(message)
+    # Отправка сообщения с запросом ввода имени
+    message = "Пожалуйста, введите ваше имя:"
+    await context.bot.send_message(chat_id=chat_id, text=message)
     return NAME
 
 
 async def name(update: Update, context: CallbackContext, from_back=False):
-    chat_id = update.message.chat_id
-    context.user_data['name'] = update.message.text
-    logging.info(f"Сработала функция name. Пользователь ввел {context.user_data['name']}")
     keyboard = [[InlineKeyboardButton("Вернуться назад", callback_data='return_to_start')]]
-
-    await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
-    await asyncio.sleep(0.5)
 
     message = "Пожалуйста, введите ваш контактный номер телефона:"
     if from_back:
         await update.callback_query.edit_message_text(message, reply_markup=InlineKeyboardMarkup(keyboard))
     else:
+        chat_id = update.message.chat_id
+        context.user_data['name'] = update.message.text
+        logging.info(f"Сработала функция name. Пользователь ввел {context.user_data['name']}")
+
+        await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+        await asyncio.sleep(0.5)
+
         await update.message.reply_text(message, reply_markup=InlineKeyboardMarkup(keyboard))
     return PHONE
 
 
 async def phone(update: Update, context: CallbackContext, from_back=False):
-    phone_number = update.message.text
-
-    if not re.match(r"^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$", phone_number):
-        await update.message.reply_text("Некорректный номер, попробуйте еще раз:")
-        return PHONE
-    context.user_data['phone'] = phone_number
-
-    logging.info(f"Сработала функция phone. Пользователь ввел {phone_number}")
-
     keyboard = [
-        [InlineKeyboardButton("Терапевт", callback_data='dentist'),
-         InlineKeyboardButton("Хирург", callback_data='surgeon')],
-        [InlineKeyboardButton("Имплантолог", callback_data='implantologist'),
-         InlineKeyboardButton("Парадонтолог", callback_data='periodontist')],
-        [InlineKeyboardButton("Профчистка", callback_data='cleaning')],
+        [InlineKeyboardButton("Терапевт", callback_data='Терапевт'),
+         InlineKeyboardButton("Хирург", callback_data='Хирург')],
+        [InlineKeyboardButton("Имплантолог", callback_data='Имплантолог'),
+         InlineKeyboardButton("Парадонтолог", callback_data='Парадонтолог')],
+        [InlineKeyboardButton("Профчистка", callback_data='Профчистка')],
         [InlineKeyboardButton("Вернуться назад", callback_data='return_phone')]
     ]
 
@@ -457,6 +460,15 @@ async def phone(update: Update, context: CallbackContext, from_back=False):
     if from_back:
         await update.callback_query.edit_message_text(message, reply_markup=InlineKeyboardMarkup(keyboard))
     else:
+        phone_number = update.message.text
+
+        if not re.match(r"^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$", phone_number):
+            await update.message.reply_text("Некорректный номер, попробуйте еще раз:")
+            return PHONE
+        context.user_data['phone'] = phone_number
+
+        logging.info(f"Сработала функция phone. Пользователь ввел {phone_number}")
+
         await update.message.reply_text(message, reply_markup=InlineKeyboardMarkup(keyboard))
     return PROCEDURE
 
@@ -467,8 +479,8 @@ async def procedure(update: Update, context: CallbackContext, from_back=False):
     context.user_data['procedure'] = query.data
     logging.info(f"Сработала функция phone. Пользователь ввел {context.user_data['name']}")
     keyboard = [
-        [InlineKeyboardButton("Доктор Игорь", callback_data='select_doctor_igor'),
-         InlineKeyboardButton("Доктор Яков", callback_data='select_doctor_jacob')],
+        [InlineKeyboardButton("Доктор Игорь", callback_data='Доктор Игорь')],
+        [InlineKeyboardButton("Доктор Яков", callback_data='Доктор Яков')],
         [InlineKeyboardButton("Вернуться назад", callback_data='return_procedure')]
     ]
     message = "Выберите доктора:"
@@ -487,11 +499,11 @@ async def doctor(update: Update, context: CallbackContext, from_back=False):
     await query.answer()
     context.user_data['doctor'] = query.data
     keyboard = [
-        [InlineKeyboardButton("Доктор Игорь", callback_data='select_doctor_igor'),
-         InlineKeyboardButton("Доктор Яков", callback_data='select_doctor_jacob')],
+        [InlineKeyboardButton("10.05.2024", callback_data='10.05.2024'),
+         InlineKeyboardButton("12.05.2024", callback_data='12.05.2024')],
         [InlineKeyboardButton("Вернуться назад", callback_data='return_doctor')]
     ]
-    message = "Выберите доктора:"
+    message = "Выберите дату приема:"
 
     logging.info(f"Сработала функция doctor. Пользователь ввел {context.user_data['doctor']}")
 
@@ -507,11 +519,11 @@ async def date(update: Update, context: CallbackContext, from_back=False):
     await query.answer()
     context.user_data['date'] = query.data
     keyboard = [
-        [InlineKeyboardButton("10.05.2024", callback_data='10.05.2024'),
-         InlineKeyboardButton("12.05.2024", callback_data='12.05.2024')],
+        [InlineKeyboardButton("10:00", callback_data='10:00'),
+         InlineKeyboardButton("11:00", callback_data='11:00')],
         [InlineKeyboardButton("Вернуться назад", callback_data='return_date')]
     ]
-    message = "Выберите дату приема:"
+    message = "Выберите время приема:"
 
     logging.info(f"Сработала функция date. Пользователь ввел {context.user_data['date']}")
 
@@ -526,12 +538,19 @@ async def time(update: Update, context: CallbackContext, from_back=False):
     query = update.callback_query
     await query.answer()
     context.user_data['time'] = query.data
+
+    user_name = context.user_data['name']
+    user_phone = context.user_data['phone']
+    procedure = context.user_data['procedure']
+    doctor = context.user_data['doctor']
+    date = context.user_data['date']
+    time = context.user_data['time']
+
     keyboard = [
-        [InlineKeyboardButton("10:00", callback_data='10:00'),
-         InlineKeyboardButton("11:00", callback_data='11:00')],
+        [InlineKeyboardButton("Подтвердить запись", callback_data='confirm')],
         [InlineKeyboardButton("Вернуться назад", callback_data='return_time')]
     ]
-    message = "Выберите время приема:"
+    message = f"Уважаемый {user_name}, Вы будете записаны к {doctor}({procedure}) на {date} в {time}"
 
     logging.info(f"Сработала функция time. Пользователь ввел {context.user_data['time']}")
 
@@ -543,6 +562,7 @@ async def time(update: Update, context: CallbackContext, from_back=False):
 
 
 async def back_to_previous(update: Update, context: CallbackContext):
+    print('Invoked: back_to_previous')
     query = update.callback_query
     await query.answer()
     data = query.data
@@ -551,8 +571,8 @@ async def back_to_previous(update: Update, context: CallbackContext):
 
     handlers = {
         'return_to_start': (start, NAME),
-        'return_phone': (phone, PHONE),
-        'return_procedure': (procedure, PROCEDURE),
+        'return_phone': (name, PHONE),
+        'return_procedure': (phone, PROCEDURE),
         'return_doctor': (doctor, DOCTOR),
         'return_date': (date, DATE),
         'return_time': (time, TIME)
@@ -575,23 +595,24 @@ def main():
         states={
             NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)],
             PHONE: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, phone),
-                CallbackQueryHandler(back_to_previous, pattern='^return_')],
+                CallbackQueryHandler(back_to_previous, pattern='^return_'),
+                MessageHandler(filters.TEXT & ~filters.COMMAND, phone)
+            ],
             PROCEDURE: [
-                CallbackQueryHandler(procedure),
-                CallbackQueryHandler(back_to_previous, pattern='^return_')
+                CallbackQueryHandler(back_to_previous, pattern='^return_'),
+                CallbackQueryHandler(procedure)
             ],
             DOCTOR: [
+                CallbackQueryHandler(back_to_previous, pattern='^return_'),
                 CallbackQueryHandler(doctor),
-                CallbackQueryHandler(back_to_previous, pattern='^return_')
             ],  # Add appropriate handlers
             DATE: [
+                CallbackQueryHandler(back_to_previous, pattern='^return_'),
                 CallbackQueryHandler(date),
-                CallbackQueryHandler(back_to_previous, pattern='^return_')
             ],  # Add appropriate handlers
             TIME: [
+                CallbackQueryHandler(back_to_previous, pattern='^return_'),
                 CallbackQueryHandler(time),
-                CallbackQueryHandler(back_to_previous, pattern='^return_')
             ],  # Add appropriate handlers
             CONFIRMATION: []  # Add appropriate handlers
         },
